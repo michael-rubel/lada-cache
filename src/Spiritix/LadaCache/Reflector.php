afdfdf56d97d452e39db5393c117af1ff2695349
valeriisotnyk <v.sotnik@temida.io>
2023-02-01 10:04:02 +0100

    check instance

--- src/Spiritix/LadaCache/Reflector.php
+++ src/Spiritix/LadaCache/Reflector.php
@@ -173,7 +173,7 @@ class Reflector
             list($table, $column) = $this->splitTableAndColumn($where['column']);
 
             // Make sure that the where clause applies for the primary key column
-            if ($column !== $this->queryBuilder->getPrimaryKeyName()) {
+            if ($this->queryBuilder instanceof QueryBuilder && $column !== $this->queryBuilder->getPrimaryKeyName()) {
                 continue;
             }
 
